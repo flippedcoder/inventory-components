154f1760c7cb051ffefb8efc43400a10c19707fb
flippedcoder <milecia.mcgregor@aol.com>
2020-11-04 16:53:41 -0600

    added mock data

---
+++ src/dataSource.js
@@ -0,0 +1,53 @@
+export const activitiesData = [
+  {
+    "name": "Got the mustard",
+    "date": new Date(),
+    "description": "Spicy jalapeno bacon ipsum dolor amet chuck velit jerky dolore chicken proident landjaeger veniam burgdoggen."
+  },
+  {
+    "name": "Added bread",
+    "date": new Date(),
+    "description": "Nisi doner incididunt, id ham chicken cow reprehenderit. Tri-tip excepteur mollit officia hamburger proident eiusmod consectetur."
+  },
+  {
+    "name": "Ran out of laundry detergent",
+    "date": new Date(),
+    "description": "Pariatur occaecat tongue chislic tail. Corned beef ad cow anim tempor ut short ribs."
+  }
+]
+
+export const alertsData = [
+  {
+    "name": "Get toilet paper",
+    "date": new Date(),
+    "description": "Spicy jalapeno bacon ipsum dolor amet chuck velit jerky dolore chicken proident landjaeger veniam burgdoggen."
+  },
+  {
+    "name": "Get new hand soap",
+    "date": new Date(),
+    "description": "Nisi doner incididunt, id ham chicken cow reprehenderit. Tri-tip excepteur mollit officia hamburger proident eiusmod consectetur."
+  },
+  {
+    "name": "Restock vegetables",
+    "date": new Date(),
+    "description": "Pariatur occaecat tongue chislic tail. Corned beef ad cow anim tempor ut short ribs."
+  }
+]
+
+export const usersData = [
+  {
+    "name": "Sand wich",
+    "date": new Date(),
+    "description": "Spicy jalapeno bacon ipsum dolor amet chuck velit jerky dolore chicken proident landjaeger veniam burgdoggen."
+  },
+  {
+    "name": "Ca Cao",
+    "date": new Date(),
+    "description": "Nisi doner incididunt, id ham chicken cow reprehenderit. Tri-tip excepteur mollit officia hamburger proident eiusmod consectetur."
+  },
+  {
+    "name": "Tsu Nami",
+    "date": new Date(),
+    "description": "Pariatur occaecat tongue chislic tail. Corned beef ad cow anim tempor ut short ribs."
+  }
+]
